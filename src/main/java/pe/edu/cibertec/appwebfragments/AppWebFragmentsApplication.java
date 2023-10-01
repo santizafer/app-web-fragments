ec1226259f34f75ae6040f3844e21fb4b44e459a
santizafer <santizafer@gmail.com>
2023-10-01 15:28:48 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebfragments/AppWebFragmentsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFragmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebfragments/AppWebFragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
